d7d1e948a9617e11ea8f6af988e223f796f2b9cb
Diana Brik <di.brik@yandex.kz>
2023-01-17 17:12:03 +0600

    Seminar 14.01.23 Task 25

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+int accumulator = 1;
+
+for (int i = 0; i < degree; i++)
+{
+    accumulator = accumulator * number;
+}
+Console.WriteLine($"{number} в степени {degree} = {accumulator}");
